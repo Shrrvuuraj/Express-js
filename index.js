09fd6405d8fbb0a30983452fba183112f04e298a
Shravan Rajput <shrrvvuurajput@gmail.com>
2025-06-18 19:11:33 +0530

    cokkie and session

--- index.js
+++ index.js
@@ -10,19 +10,44 @@ app.use(express.json());
 app.use(
   session({
     secret: "sample-1",
-    resave:false,
-    saveUninitialized:false
+    resave: false,
+    saveUninitialized: false,
   })
 );
-const users=[]
+const users = [];
 
-app.get("/", (req, res) => {
-  res.send("hello");
+// app.get("/", (req, res) => {
+//   res.send("hello");
+// });
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  users.push({
+    username,
+    password
+  });
+  res.send("user registered")
 });
 
-app.post("/register",async (req,res)=>{
- const {username,password}=req.body
- username.push
+app.post("/login",async (req,res)=>{
+  const{username,password}=req.body;
+  const user=users.find(u=>u.username===username);
+  if (!user||password!==user.password) {
+    return res.send("not authorized")
+    
+  }
+  req.session.user=user;
+  res.send("user logged in")
+
+})
+
+app.get("/dashboard" ,(req,res)=>{
+  if(!req.session.user){
+    return res.send(`Logg in pls ${req.session} `)
+
+  }
+  res.send(`welcome ${req.session.user.username}`)
+
 })
 
 app.listen(PORT, () => {
